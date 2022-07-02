4ef6682ecfac4c905c48bbbb514871326b46ecb4
Draga929 <ivan@bublikov.com>
2022-07-02 20:38:49 +0300

    add file

--- Script_HW_1_Terminal.sh
+++ Script_HW_1_Terminal.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-mkdir Proga
-cd Proga
+mkdir SCRIPT
+cd SCRIPT
 mkdir D_4 D_5 D_6
 cd D_4
 touch f_6.txt f_7.txt f_8.txt f_9.json f_10.json
 mkdir F_4 F_5 F_6
 ls -la
 mv f_6.txt f_7.txt F_4
-
